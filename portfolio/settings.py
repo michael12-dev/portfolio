7a04d3f7775c2dfd532a3d31e9db24f3d8578eec
nzere.michael <nzeremichael2020@gmail.com>
2024-12-06 18:11:57 +0100

    Configure static files and WhiteNoise

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-$ytvsnw4v+uzav4t!b$v-t%(6cw)j$@hfzc=3g&agx!v!*37u9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-nq96.onrender.com', 'localhost']
+# ALLOWED_HOSTS = ['portfolio-nq96.onrender.com', 'http://127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -112,15 +113,29 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[STATIC_DIR,]
+# Specify your development static directory here
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+# Specify a separate directory for collectstatic to collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Security settings
+ALLOWED_HOSTS = ['portfolio-nq96.onrender.com', '127.0.0.1', 'localhost']  # Add your Render domain here
+
+# Middleware to serve static files in production
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+
+# WhiteNoise storage for static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
